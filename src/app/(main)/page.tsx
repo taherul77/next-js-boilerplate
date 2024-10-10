22ee3a122dadaf11018833725af7532db44094ac
taherul77 <taherul2317@gmail.com>
2024-10-11 00:46:53 +0600

    add setup

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="bg-red-500 flex justify-center items-center w-full h-screen text-4xl font-bold uppercase text-white">
+      template
+    </div>
+  );
+};
+
+export default page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,12 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      animation: {
+        "slide-in-left": "slide-in-left 0.5s ease-out",
       },
     },
   },
   plugins: [],
 };
-export default config;
