2b95ab384d04a1c236f4e1925446df641d59bec6
taherul77 <taherul2317@gmail.com>
2024-10-11 00:53:00 +0600

    update project

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div className="bg-red-500 flex justify-center items-center w-full h-screen text-4xl font-bold uppercase text-white">
-      template
+      template ui
     </div>
   );
 };
